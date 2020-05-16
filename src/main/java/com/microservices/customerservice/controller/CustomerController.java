c0820fc082835d7fa347a1595b35220678e3a90a
Vinoth Kumar <vinothg2309@gmail.com>
2020-05-16 10:20:51 +0530

    initial commit

---
+++ src/main/java/com/microservices/customerservice/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package com.microservices.customerservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() throws InterruptedException {
+		Thread.sleep(10000);
+		return "Inside Customer service";
+	}
+	
+}
