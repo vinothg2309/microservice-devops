47f092b4c4cbce960170bf928b0b7affd18e6c00
vinothg2309 <vinothg2309@gmail.com>
2020-05-18 18:10:36 +0530

    Update CustomerController.java

--- src/main/java/com/microservices/customerservice/controller/CustomerController.java
+++ src/main/java/com/microservices/customerservice/controller/CustomerController.java
@@ -10,7 +10,7 @@ public class CustomerController {
 	
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test() throws InterruptedException {
-		Thread.sleep(10000);
+		//Thread.sleep(10000);
 		return "Inside Customer service";
 	}
 	
